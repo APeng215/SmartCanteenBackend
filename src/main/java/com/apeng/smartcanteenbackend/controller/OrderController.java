34e93fb06916e4da8385a66fbf7dc0f05ec87159
A_Peng215 <976310604@qq.com>
2024-04-28 21:11:30 +0800

    Add get-order endpoint

--- src/main/java/com/apeng/smartcanteenbackend/controller/OrderController.java
+++ src/main/java/com/apeng/smartcanteenbackend/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.apeng.smartcanteenbackend.controller;
 
 import com.alibaba.fastjson2.JSON;
-import com.apeng.smartcanteenbackend.config.WebSecurityConfig;
 import com.apeng.smartcanteenbackend.controller.util.OrderAddingRequest;
 import com.apeng.smartcanteenbackend.entity.Dish;
 import com.apeng.smartcanteenbackend.entity.Order;
@@ -9,11 +8,7 @@ import com.apeng.smartcanteenbackend.entity.User;
 import com.apeng.smartcanteenbackend.repository.DishRepository;
 import com.apeng.smartcanteenbackend.repository.OrderRepository;
 import com.apeng.smartcanteenbackend.repository.UserRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -38,9 +33,30 @@ public class OrderController {
         OrderAddingRequest orderAddingRequest = JSON.parseObject(body, OrderAddingRequest.class);
         List<Dish> orderedDishes = new ArrayList<>();
         dishRepository.findAllById(orderAddingRequest.dishIds()).forEach(orderedDishes::add);
-        Order orderAdded = new Order(null, orderedDishes, userRepository.findById(currentUser.getName()).get());
+        Order orderAdded = new Order(null, orderedDishes, getCurrentUser(currentUser));
         orderRepository.save(orderAdded);
         return "Adding order successfully: " + orderAdded;
+    }
+
+    private User getCurrentUser(Principal currentUser) {
+        return userRepository.findById(currentUser.getName()).get();
+    }
+
+    @GetMapping
+    private String getOrders(Principal currentUser) {
+        List<Order> orders = findOrdersByUser(currentUser);
+        hideUserInfo(orders);
+        return JSON.toJSONString(orders);
+    }
+
+    private List<Order> findOrdersByUser(Principal currentUser) {
+        return orderRepository.findByUser(getCurrentUser(currentUser));
+    }
+
+    private static void hideUserInfo(List<Order> orders) {
+        orders.forEach(order -> {
+            order.setUser(null);
+        });
     }
 
 }
--- src/main/java/com/apeng/smartcanteenbackend/repository/OrderRepository.java
+++ src/main/java/com/apeng/smartcanteenbackend/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.apeng.smartcanteenbackend.repository;
 
 import com.apeng.smartcanteenbackend.entity.Order;
+import com.apeng.smartcanteenbackend.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> findByUser(User user);
+
 }
